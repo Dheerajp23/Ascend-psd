86dafab195a8408ccf54721c6f3afed7d1f2d5f8
Dheerajp23 <59532632+Dheerajp23@users.noreply.github.com>
2025-01-26 02:14:08 +0530

    changes and complete

--- text.js
+++ text.js
@@ -1,3 +1,21 @@
 
+document.querySelector(".header-button").addEventListener('click', function () {
+  const mobile=   document.querySelector(".mobile");
+  if(mobile.style.display === "none" || mobile.style.display === ""){
+    mobile.style.display = "block";
+}
+    else{
+      mobile.style.display = "none";
+    }
+});
+
+
+    const nav = document.querySelectorAll(".mobile ul li a");
+    nav.forEach((item) => {
+      item.addEventListener('click', function () {
+        const mobile =   document.querySelector(".mobile");
+        mobile.style.display = "none";
+      });
+    });
 
 
